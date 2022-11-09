9403226d5a247cd6997d70d04f0b1ccc2365197f
Sunday <65029229+ndagnhat@users.noreply.github.com>
2022-11-09 22:23:51 +0700

    Create config.ts

---
+++ .vitepress/config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vitepress';
+
+// refer https://vitepress.vuejs.org/config/introduction for details
+export default defineConfig({
+  lang: 'en-US',
+  title: 'VitePress',
+  description: 'Vite & Vue powered static site generator.',
+
+  themeConfig: {
+    nav: [
+      { text: 'Example', link: '/example' },
+      { text: 'Example1', link: '/example1' },
+
+      // {
+      //   text: 'Dropdown Menu',
+      //   items: [
+      //     { text: 'Item A', link: '/item-1' },
+      //     { text: 'Item B', link: '/item-2' },
+      //     { text: 'Item C', link: '/item-3' },
+      //   ],
+      // },
+
+      // ...
+    ],
+
+    sidebar: [
+      {
+        // text: 'Guide',
+        items: [
+          { text: 'Example', link: '/example' },
+          // ...
+        ],
+      },
+    ],
+  },
+});
